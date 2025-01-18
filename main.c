e63f1f17d2d75913853a8ef0ba1b5c2245dba6e1
Carlos Eduardo da Costa <costa25102002@gmail.com>
2025-01-18 15:49:10 -0300

    Atualizando o main com o modulo de Transacao

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "bibliotecas/caixa.h"
 #include "bibliotecas/utils.h"
 #include "bibliotecas/cliente.h"
 #include "bibliotecas/veiculo.h"
@@ -9,29 +11,33 @@
 #include "bibliotecas/peca.h"
 #include "bibliotecas/funcionario.h"
 #include "bibliotecas/servico.h"
+#include "bibliotecas/moduloTransacao.h"
 
-int main(void)
-{
+int main(void) {
     setTipoArquivo(lerInt("Digite com qual tipo de dados vc deseja trabalhar: "));
+
 
     int opc = 0;
 
-    Oficina* oficina_atual = migraDadosOficina();
-    Cliente* cliente = NULL;
-        cliente =migraDadosCliente();
-    Veiculo* veiculo = migraDadosVeiculo();
-    Peca* pecas = migraDadosPeca();
-    Fornecedor* fornercedor = migraDadosFornecedor();
-    Servico* servico = migraDadosServicos();
-    Funcionario* funcionario = migraDadosFuncionario();
+    Oficina *oficina_atual = migraDadosOficina();
+    Cliente *cliente = NULL;
+    cliente = migraDadosCliente();
+    Veiculo *veiculo = migraDadosVeiculo();
+    Peca *pecas = migraDadosPeca();
+    Fornecedor *fornercedor = migraDadosFornecedor();
+    Servico *servico = migraDadosServicos();
+    Funcionario *funcionario = migraDadosFuncionario();
+    Transacao *transacoes = migraDadosTransacao();
+    ContasPagar *contas_pagar = migraDadosContasPagar();
+    ContasReceber *contas_receber = migraDadosContasReceber();
+    float valor_em_caixa = 0;
+    valor_em_caixa = migraDadosCaixa();
 
 
-    while (opc != 7)
-    {
-        if(getTipoArquivo() != MEM) oficina_atual = readOficina();
+    while (opc != 7) {
+        if (getTipoArquivo() != MEM) oficina_atual = readOficina();
         //verifica se a oficina existe oficina
-        //if (oficina_atual != NULL)
-        //{
+        if (oficina_atual != NULL) {
             printf("\n===============SISTEMA GERENCIAL DE UMA OFICINA===============\n\n \t\tSeja bem vindo!! %s \n\n",
                    oficina_atual->nome);
             opc = lerInt(
@@ -44,64 +50,82 @@ int main(void)
                 "      \t\t\t 6- IMPORTAÇÃO/EXPORTAÇÃO\n"
                 "      \t\t\t 7-SAIR\n"
                 "=>");
-            switch (opc)
-            {
+            switch (opc) {
                 //aqui vai chamar para cadastro...
-            case 1:
-                int cadastros = lerInt("\n=========MODULOS CADASTROS==============\n"
-                    "\t DIGITE UMA OPÇÃO:\n"
-                    "      \t\t\t 1- CLIENTES\n"
-                    "      \t\t\t 2- VEICULOS\n"
-                    "      \t\t\t 3- PEÇAS\n"
-                    "      \t\t\t 4- FORNECEDORES\n"
-                    "      \t\t\t 5- SERVIÇOS\n"
-                    "      \t\t\t 6- FUNCIONARIOS\n"
-                    "      \t\t\t 7- VOLTAR\n"
-                    "=>");
-                switch (cadastros)
-                {
                 case 1:
-                    menuCliente(&cliente);
+                    int cadastros = lerInt("\n=========MODULOS CADASTROS==============\n"
+                        "\t DIGITE UMA OPÇÃO:\n"
+                        "      \t\t\t 1- CLIENTES\n"
+                        "      \t\t\t 2- VEICULOS\n"
+                        "      \t\t\t 3- PEÇAS\n"
+                        "      \t\t\t 4- FORNECEDORES\n"
+                        "      \t\t\t 5- SERVIÇOS\n"
+                        "      \t\t\t 6- FUNCIONARIOS\n"
+                        "      \t\t\t 7- OFICINA\n"
+                        "      \t\t\t 0- VOLTAR\n"
+                        "=>");
+                    switch (cadastros) {
+                        case 1:
+                            menuCliente(&cliente);
+                            break;
+                        case 2:
+
+                            menuVeiculo(&veiculo);
+                            break;
+                        case 3:
+                            //para  pecas preciso passar fornecedor e pecas
+
+                            menuPecas(&pecas, &fornercedor);
+                            break;
+                        case 4:
+                            menuFornecedor(&fornercedor);
+                            break;
+                        case 5:
+                            menuServicos(&servico);
+                            break;
+                        case 6:
+                            menuFuncionario(&funcionario);
+                            break;
+                        case 7:
+                            menuOficina(&oficina_atual);
+                            break;
+                        default:
+                            opc = -1;
+                            break;
+                    }
+
+
                     break;
                 case 2:
-
-                menuVeiculo(&veiculo);
                     break;
                 case 3:
-                    /* para  pecas preciso passar fornecedor e pecas */
-
-                    menuPecas(&pecas,&fornercedor);
                     break;
                 case 4:
-                    menuFornecedor(&fornercedor);
+                    int cadastrosF = lerInt("\n=========MODULOS FINANCEIRO==============\n"
+                        "\t DIGITE UMA OPÇÃO:\n"
+                        "      \t\t\t 1- TRANSAÇÃO\n"
+                        "      \t\t\t 2- CONTAS A PAGAR\n"
+                        "      \t\t\t 3- CONTAS A RECEBER\n"
+                        "=>");
+                    switch (cadastrosF) {
+                        case 1:
+                            menuTransacoes(&transacoes);
+                            break;
+                        case 2:
+                            menuContasPagar(&contas_pagar, transacoes, &valor_em_caixa);
+                            break;
+                        case 3:
+                            menuContasReceber(&contas_receber, &valor_em_caixa);
+                            break;
+                    }
                     break;
                 case 5:
-                    menuServicos(&servico);
                     break;
-                    case 6:
-                    menuFuncionario(&funcionario);
-                    break;
-                default:
-                    break;
-                }
-
-
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
             }
-        //}
-        /*else
-        {
-            Oficina* oficina_nova = migraDadosOficina();
+        } else {
+            Oficina *oficina_nova = migraDadosOficina();
             menuOficina(&oficina_nova);
-        }*/
+        }
     }
 
     return 0;
